283dea6774489d531bfddd0d78e2d081d5a24cf3
kamkoum-sabrine <kamkoumsabrine30@gmail.com>
2022-11-09 09:23:42 +0100

    create UserRepository service

---
+++ src/main/java/com/example/DAO/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.DAO;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+	@Query(" select u from User u where u.username = ?1")
+	 Optional
+	 <User> findUserWithName(String username);
+} 
